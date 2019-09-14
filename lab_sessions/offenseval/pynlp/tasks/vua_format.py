1822d3e5fa93330e2c20421a5b647594c73e8324
IsaMaks <e.maks@vu.nl>
2019-09-14 12:55:13 +0200

    Update vua_format.py
    changes in def load because of data format VUA

--- lab_sessions/offenseval/pynlp/tasks/vua_format.py
+++ lab_sessions/offenseval/pynlp/tasks/vua_format.py
@@ -6,23 +6,26 @@ class VuaFormat:
 
     def __init__(self):
         self.training_file = 'trainData.csv'
-        self.dev_file = 'devData.csv'
+        self.test_file = 'testData.csv'
         self.task = None
         self.name = "VUA_format"
         self.train_data = None
-        self.test_data = []
-        self.dev_data = None
+        self.test_data = None
+        self.dev_data = []
 
     def __str__(self):
         return self.name + ", " + self.task
 
+
+            
     def load(self, data_dir, test_files=[]):
         self.train_data = pd.read_csv(data_dir + self.training_file, delimiter="\t")
-        self.dev_data = pd.read_csv(data_dir + self.dev_file, delimiter="\t", header=None,
-                                    names=["Id", "Text", "Label"])
+        #self.dev_data = pd.read_csv(data_dir + self.dev_file, delimiter="\t", header=None,names=["Id", "Text", "Label"])
+        #self.test_data = pd.read_csv(data_dir + self.test_file, delimiter="\t", header=None,names=["Id", "Text", "Label"])
+        self.test_data = pd.read_csv(data_dir + self.test_file, delimiter="\t")
 
-        for f in test_files:
-            self.test_data.append(pd.read_csv(data_dir + f, delimiter="\t"))
+        #for f in test_files:
+            #self.test_data.append(pd.read_csv(data_dir + f, delimiter="\t"))
 
     def train_instances(self):
         """returns training instances and labels for a given task
